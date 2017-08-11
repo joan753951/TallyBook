c0fc28377f8952a870c87ebdda892c852a90b6c9
范瓊文 <joan753951@tunghosteel.com>
2017-08-11 15:24:53 +0800

    第二次簽入

---
+++ TallyBook/TallyBook/Controllers/TallyBookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TallyBook.Models.ViewModels;
+
+namespace TallyBook.Controllers
+{
+    public class TallyBookController : Controller
+    {
+        // GET: TallyBook
+        public ActionResult TallyBook()
+        {
+            return View();
+        }
+        public ActionResult TallyBookChildAction()
+        {
+            List<TallyBookViewModel> TallyBookData = new List<TallyBookViewModel>
+            {
+                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(1), Money = 1000},
+                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(2), Money = 2000 },
+                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(3), Money = 3000 },
+                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(4), Money = 4000 },
+                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(5), Money = 5000 },
+            };
+            var viewModel = new TallyBookViewModel
+            {
+                TallyBook = TallyBookData
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ TallyBook/TallyBook/Models/ViewModels/TallyBookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TallyBook.Models.ViewModels
+{
+    public class TallyBookViewModel
+    {
+        public List<TallyBookViewModel> TallyBook { get; set; }
+
+        [DisplayName("類別")]
+        public string Type { get; set; }
+
+        [DisplayName("金額")]
+        public int Money { get; set; }
+
+        [DisplayName("日期")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("備註")]
+        public string Remark { get; set; }
+    }
+}
