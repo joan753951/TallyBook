e345919c3a9c6071ba8ef8e12fd67756eecf27b3
范瓊文 <joan753951@tunghosteel.com>
2017-08-17 10:48:03 +0800

    顯示DB的值、實做Service層

--- TallyBook/TallyBook/Controllers/TallyBookController.cs
+++ TallyBook/TallyBook/Controllers/TallyBookController.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TallyBook.Models;
 using TallyBook.Models.ViewModels;
 
 namespace TallyBook.Controllers
 {
     public class TallyBookController : Controller
     {
+
+        private readonly AccountBookService _accountbookSvc;
+
+        public TallyBookController()
+        {
+            _accountbookSvc = new AccountBookService();
+        }
+
         // GET: TallyBook
         public ActionResult TallyBook()
         {
@@ -16,19 +25,7 @@ namespace TallyBook.Controllers
         }
         public ActionResult TallyBookChildAction()
         {
-            List<TallyBookViewModel> TallyBookData = new List<TallyBookViewModel>
-            {
-                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(1), Money = 1000},
-                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(2), Money = 2000 },
-                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(3), Money = 3000 },
-                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(4), Money = 4000 },
-                new TallyBookViewModel() { Type = "支出", Date = DateTime.Now.AddDays(5), Money = 5000 },
-            };
-            var viewModel = new TallyBookViewModel
-            {
-                TallyBook = TallyBookData
-            };
-            return View(viewModel);
+            return View(_accountbookSvc.QueryAll());
         }
     }
 }
---
+++ TallyBook/TallyBook/Models/AccountBookService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TallyBook.Models.ViewModels;
+
+namespace TallyBook.Models
+{
+    public class AccountBookService
+    {
+        private readonly SkillTreeHomeworkEntities _db;
+
+        public AccountBookService()
+        {
+            _db = new SkillTreeHomeworkEntities();
+        }
+
+        public virtual TallyBookViewModel QueryAll()
+        {
+            TallyBookViewModel result = new TallyBookViewModel();
+            var selector = _db.AccountBook
+                .Select(p => new TallyBookViewModel()
+                {
+                    Type = (p.Categoryyy.ToString() == "1" ? "支出" : "收入"),
+                    Date = p.Dateee,
+                    Money = p.Amounttt,
+                    Remark = p.Remarkkk
+                });
+            result.TallyBook = selector.AsNoTracking()
+                                .ToList();
+            return result;
+        }
+    }
+}
